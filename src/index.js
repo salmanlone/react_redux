3dbe61d4bbfe5e0a66900d1412117d77cb05950b
Salman Tariq Lone <salmanlone89@gmail.com>
2019-06-10 17:03:20 +0800

    Redux in its simplest form

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import index from "./js/index"
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,10 +1,18 @@
 // src/js/reducers/index.js
-const initialState = {
-    articles: []
-  };
-  
-  function rootReducer(state = initialState, action) {
-    return state;
-  };
 
-  export default rootReducer;
+import { ADD_ARTICLE } from '../constants/action-types';
+
+const initialState = {
+  articles: []
+};
+
+function rootReducer(state = initialState, action) {
+  if (action.type === ADD_ARTICLE) {
+    return Object.assign({}, state, {
+      articles: state.articles.concat(action.payload)
+    });
+  }
+  return state;
+}
+
+export default rootReducer;
