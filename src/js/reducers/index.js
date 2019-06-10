ee2d597ad36d6d70f64a60a379e64d80755026e9
Salman Tariq Lone <salmanlone89@gmail.com>
2019-06-10 16:30:06 +0800

    sate and reducer added for redux

---
+++ src/js/reducers/index.js
@@ -0,0 +1,10 @@
+// src/js/reducers/index.js
+const initialState = {
+    articles: []
+  };
+  
+  function rootReducer(state = initialState, action) {
+    return state;
+  };
+
+  export default rootReducer;
