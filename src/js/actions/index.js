898cecc9f03815dbe75910187470a1701658e438
Salman Tariq Lone <salmanlone89@gmail.com>
2019-06-13 15:58:38 +0800

    introduced saga watcher and worker for api call

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -11,11 +11,5 @@ export function foundBadWord(payload) {
 };
 
 export function getData() {
-  return function (dispatch) {
-    return fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(response => response.json())
-      .then(json => {
-        dispatch({ type: "DATA_LOADED", payload: json });
-      });
-  };
+  return { type: "DATA_REQUESTED" };
 }
